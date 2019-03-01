3d497e53cfd281350f6ef1e6ca316c788d509c71
Lige <lige.williamson@gmail.com>
2019-03-01 15:39:59 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,16 @@
+require 'pry'
 def consolidate_cart(cart)
   # code here
+  consolodated = {}
+  cart.each do |item|
+    item.map do |product, data|
+      item[product][:count] = cart.count(item)
+      if !consolodated.has_key?(product)
+        consolodated[product] = data
+      end
+      binding.pry
+    end
+  end
 end
 
 def apply_coupons(cart, coupons)
